35a593f9d7658233cf00ed36d57ddc10d7acd720
DemineyDev <demineydev@gmail.com>
2018-10-11 09:39:31 +0200

    Committing my RomanNumeralConverter

---
+++ main.js
@@ -0,0 +1,27 @@
+var romanNumerals = [
+			[1000, "M"],
+			[900, "CM"],
+			[500, "D"],
+			[400, "CD"],
+			[100, "C"],
+			[90, "XC"],
+			[50, "L"],
+			[40, "XL"],
+			[10, "X"],
+			[9, "IX"],
+			[5, "V"],
+			[4, "IV"],
+			[1, "I"]
+]
+
+function convertToRoman(num) {
+	if (num === 0) {
+		return ""
+	}
+	for (var i = 0; i < romanNumerals.length; i++) {
+		if (num >= romanNumerals[i][0]) {
+			return romanNumerals[i][1] + convertToRoman(num - romanNumerals[i][0])
+		}
+	}
+
+}
